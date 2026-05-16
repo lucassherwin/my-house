d17e4d4ea45f0659913f23150729ff5131ea9b72
Lucas Sherwin <lksherwin@gmail.com>
2026-05-15 20:22:16 -0400

    Removes test comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
   private
 
   def current_person
-    # test asdfasdfasdfasdfasdf
     return @current_person if defined?(@current_person)
 
     @current_person = session[:person_id] && Person.find_by(id: session[:person_id])
