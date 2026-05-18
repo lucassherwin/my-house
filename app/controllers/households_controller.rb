8417eb1d1ff45200489641ea139deed509b3724a
Lucas Sherwin <lksherwin@gmail.com>
2026-05-17 20:56:15 -0400

    Formatting and comments

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -11,10 +11,13 @@ class HouseholdsController < ApplicationController
     end
 
     household = Household.new(household_params)
+
+    # both must save successfully or rollback
     ActiveRecord::Base.transaction do
       household.save!
       current_person.update!(household: household)
     end
+
     render json: { id: household.id, name: household.name }, status: :created
   rescue ActiveRecord::RecordInvalid => e
     render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
@@ -22,9 +25,11 @@ class HouseholdsController < ApplicationController
 
   def show
     @household = current_person.household
+
     if @household.nil? || @household.id != params[:id]
       return redirect_to(current_person.household ? household_path(current_person.household) : new_household_path)
     end
+
     @props = { household: { id: @household.id, name: @household.name } }
   end
 
